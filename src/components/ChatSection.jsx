72861d372d4181e23762567f366313cc88f1c105
lolosaisa <lwamaitha3924@gmail.com>
2024-11-14 14:46:10 +0300

    added the text gen to text promt function

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -12,6 +12,21 @@ const ChatSection = () => {
   ]);
   const [loading, setLoading] = useState(false);
 
+  async function textGenTextOnlyPrompt() {
+    // [START text_gen_text_only_prompt]
+    // Make sure to include these imports:
+    // import { GoogleGenerativeAI } from "@google/generative-ai";
+    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  
+    const prompt = "Write a story about a magic backpack.";
+  
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    // [END text_gen_text_only_prompt]
+  }
+  
+
 
 
   const handleSend = async () => {
